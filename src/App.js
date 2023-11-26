cd5fd45738dbef17495050a0dbce41f87bf6323f
pablo-aguirre <pablofabricio.aguirreguaman@studenti.unimi.it>
2023-11-26 20:11:46 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function AppTitle() {
+    return <h1>ToDo App</h1>;
 }
 
-export default App;
+function AppMessage() {
+    return <p>Benvenuto!</p>;
+}
+
+export default function App() {
+    return (
+        <div>
+            <AppTitle/>
+            <AppMessage/>
+        </div>
+    );
+}
